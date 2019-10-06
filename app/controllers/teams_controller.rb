b4d7a04c755b4abcf2dcc4ecd6264d97788a0c30
Jerome <info@jcjp.fr>
2019-10-06 14:39:33 -0400

    add update in view and controller of the teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,38 @@
 class TeamsController < ApplicationController
+  before_action :set_team, only: %i[show update]
   def index
-    @teams = Team.all
+    if params[:team].present?
+      @teams = Team.where("name ILIKE ?", "%#{params[:team]}%")
+    else
+      @teams = Team.all.order(id: :asc)
+    end
   end
 
   def edit
   end
 
+  def show
+  end
+
   def update
+    @team.name = team_params[:name]
+    @team.logo = team_params[:logo]
+    @team.name2 = team_params[:name2]
+    @team.name3 = team_params[:name3]
+    if @team.save
+      redirect_to team_path(@team)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name, :logo, :name2, :name3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :matches, only: %i[index]
   resources :pronostics, only: %i[index]
   resources :championships, only: %i[index]
-  resources :teams, only: %i[index]
+  resources :teams, only: %i[index show update]
   resources :recommendations, only: %i[index]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
